c7ecd980d7711e41f73b5c07516c7dc5fd5bf7c3
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-19 15:31:05 -0400

    Created file and added functions
    Need to complete functions and main function calls.

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insert(int array, int rightIndex, int value){
+
+
+}
+
+void selectionSort(int array[]){
+  int i;
+
+  for(i = 1; i < sizeof(array); i++){
+    insert(array, i - 1, array[i]);
+  }
+  
+}
+
+
+int main(){
+  int size, i, j, input;
+  
+  
+}
