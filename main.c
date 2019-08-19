6eb14b11b6d56ccfd582dc8697708222ab3baee6
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-19 17:38:26 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 
 void insert(int array, int rightIndex, int value){
-
+  int i;
+  
+  for(i = rightIndex; i >= 0 && array[i] > value; i--){
+    array[i + 1] = array[i];
+  }
+  
+  array[i + 1] = value;
 
 }
 
@@ -15,9 +21,26 @@ void selectionSort(int array[]){
   
 }
 
+void print(array){
+  int i;
+  for(i = 0; i < sizeof(array); i++){
+    printf("%d ", array[i]);
+  }
+}
 
 int main(){
   int size, i, j, input;
   
+  printf("Input array Size: ");
+  scanf("%d", &size);
   
+  int array[size];
+  
+  for(i = 0; i < size; i++){
+    scanf("%d", &input);
+     array[i] = input;
+  }
+  
+  selectionSort(array);
+  print(array);
 }
